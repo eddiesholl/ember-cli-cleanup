721bf2c70f99968ebbe33d88a6272972900254c4
Eddie Sholl <eddie.sholl@cooltrax.com>
2016-01-22 16:33:42 +1100

    REF-118 Small additions getting started on implementing the cleanup command

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
 
+var commands = {
+  'cleanup': require('./lib/cleanup')
+}
 module.exports = {
-  name: 'ember-cli-cleanup'
+  name: 'ember-cli-cleanup',
+
+  blueprintsPath: function () {
+    return path.join(__dirname, 'blueprints');
+  },
+
+  includedCommands: function () {
+    return commands;
+  },
+  
+  preBuild: function (args) {
+    //console.log('preBuild ' + args.length);
+  },
+  
+  
 };
